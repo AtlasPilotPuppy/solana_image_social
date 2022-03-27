50e556ec7857b4239e868e275e3d92959b64f740
Anant <anant.asty@gmail.com>
2022-03-26 18:49:31 -0600

    initial commit

---
+++ programs/social/src/lib.rs
@@ -0,0 +1,71 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod social {
+    use super::*;
+    pub fn create_user(ctx: Context<InitializeUser>, handle: String) -> Result<()> {
+        ctx.accounts.user.handle = handle;
+        ctx.accounts.user.authority = *ctx.accounts.authority.key;
+        ctx.accounts.user.bump = *ctx.bumps.get("user").unwrap();
+        Ok(())
+    }
+
+    pub fn create_user_month(ctx: Context<InitializeUserMonth>, year: u16, month: u8) -> Result<()> {
+        Ok(())
+    }
+
+}
+
+#[derive(Accounts)]
+#[instruction(bump: u8)]
+pub struct InitializeUser<'info> {
+    #[account(
+        init,
+        seeds = [b"user".as_ref(), authority.key().as_ref()],
+        bump,
+        payer=authority,
+        space = User::LEN
+    )]
+    user: Account<'info, User>,
+    #[account(mut)]
+    authority: Signer<'info>,
+    /// CHECK: we dont read from this
+    system_program: AccountInfo<'info>
+}
+
+#[derive(Accounts)]
+#[instruction(bump: u8)]
+pub struct InitializeUserMonth<'info> {
+    #[account(
+        init,
+        seeds = [b"user_month".as_ref(),authority.key().as_ref()],
+        bump,
+        payer=authority,
+        space=128
+    )]
+    user_month: Account<'info, UserMonth>,
+    #[account(mut)]
+    authority: Signer<'info>,
+    /// CHECK: We dont neeed to worry about this
+    system_program: AccountInfo<'info>
+}
+
+#[account]
+pub struct User {
+    authority: Pubkey,
+    bump: u8,
+    handle: String    
+}
+
+impl User {
+    const LEN: usize = 32 + 1 + (4*30);
+}
+#[account]
+pub struct UserMonth {
+    authority: Pubkey,
+    bump: u8,
+    year: u16,
+    month: u8
+}
---
+++ tests/social.ts
@@ -0,0 +1,81 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { assert } from "chai";
+import { Social } from "../target/types/social";
+const { PublicKey } = anchor.web3;
+
+describe("social", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.Social as Program<Social>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const authority = program.provider.wallet.publicKey;
+    const [user, bump] = await PublicKey.findProgramAddress(
+      [Buffer.from("user"),authority.toBuffer()],
+      program.programId
+    );
+    console.log("USERu " + user)    
+    console.log(`user bump: ${bump}`)
+    const tx = await program.methods.createUser("My User").accounts({
+        user,
+        authority,
+        systemProgram: anchor.web3.SystemProgram.programId,
+    }).rpc();
+    console.log("Your transaction signature", tx);
+
+  });
+
+  it("Can not re-create user", async () => {
+    const authority = program.provider.wallet.publicKey;
+    const [user, bump] = await PublicKey.findProgramAddress(
+      [Buffer.from("user"),authority.toBuffer()],
+      program.programId
+    );
+    try{
+    const tx = await program.rpc.createUser("My User", {
+      accounts: {
+        user,
+        authority,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+    });
+    console.log("Your transaction signature", tx);        
+  } catch (e) {
+    assert.ok(String(e).startsWith("Error: failed to send transaction"))
+  }
+  })
+
+  it("Can create user month accounts", async () => {
+    const authority = program.provider.wallet.publicKey;
+    const [user, _bump] = await PublicKey.findProgramAddress(
+      [Buffer.from("user"),authority.toBuffer()],
+      program.programId
+    );
+    console.log("RECOVERED: "+ user)
+    const date = new Date();
+    const [userMonth, bump] = await PublicKey.findProgramAddress(
+      [
+        Buffer.from("user_month"),
+        authority.toBuffer(),
+      ],
+      program.programId
+    );
+    const tx = await program.methods
+    .createUserMonth(date.getFullYear(), date.getMonth())
+    .accounts({
+      userMonth:userMonth,
+      authority:authority,
+      //user:user,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }
+    ).rpc()
+    console.log("Made call")
+    const um = await program.account.userMonth.fetch(userMonth);
+    console.log("Fetched userMonth")
+    console.log(um)
+  })
+
+});
