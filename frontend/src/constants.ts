90c12abeb7a990ff8fad330ca6b8982dde16882a
Anant <anant.asty@gmail.com>
2022-04-12 15:12:12 -0600

    added script to set correct program id

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,7 +1,7 @@
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { PhantomWalletAdapter, SlopeWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter, LedgerWalletAdapter, SolletWalletAdapter, SolletExtensionWalletAdapter } from "@solana/wallet-adapter-wallets";
 
-export const PROGRAM_ID = "6yT4AFpityTwxz7AdVrSJTckGyPyrXL8MKuutmBbDTnj";
+export const PROGRAM_ID = "7p8C6xyHkFbexknPCjm1KbGmVgd1mbP4GRPKt7q9HLbn";
 export const network = WalletAdapterNetwork.Devnet;
 
 export const supportedWallets = [
